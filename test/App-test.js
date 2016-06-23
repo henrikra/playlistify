f0cc088d5f11bb766d8a8a8c27edc7daad9c26be
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-23 22:41:42 +0300

    Add SearchBar component and test for it in App-test

--- test/App-test.js
+++ test/App-test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import App from '../src/components/App';
+import SearchBar from '../src/components/SearchBar';
 
 describe('App', () => {
+  const wrapper = shallow(<App />);
+
   it('renders', () => {
-    expect(shallow(<App />)).to.exist;
+    expect(wrapper).to.exist;
+  });
+
+  it('contains SearchBar', () => {
+    expect(wrapper).to.contain(<SearchBar />);
   });
 });
