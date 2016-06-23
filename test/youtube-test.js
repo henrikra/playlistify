1bb516b5263fffa4842e963b5f07794915f4465f
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 02:11:32 +0300

    Add new test for youtube util

--- test/youtube-test.js
+++ test/youtube-test.js
@@ -3,9 +3,15 @@ import { expect } from 'chai';
 import { searchYouTube } from '../src/utils/youtube';
 
 describe('YouTube util', () => {
-  it('gets 20 search results with general search term', () => {
+  it('returns 20 results with general search term', () => {
     searchYouTube('music', videos => {
       expect(videos).to.have.length(20);
+    });
+  });
+
+  it('returns zero results with nonsense search term', () => {
+    searchYouTube('thisIsNonsenseSearchTerm', videos => {
+      expect(videos).to.be.empty;
     });
   });
 });
