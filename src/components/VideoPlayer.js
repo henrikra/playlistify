c3c9ea3656b0a4c928493e6b800bb46aa81f0aab
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 22:21:11 +0300

    Add onReady function where we are binding player element to internal state

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,16 +3,31 @@ import YouTube from 'react-youtube';
 import { connect } from 'react-redux';
 
 export class VideoPlayer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onReady = this.onReady.bind(this);
+    this.state = {player: null};
+  }
+
+  onReady(e) {
+    this.setState({player: e.target});
+  }
+
   render() {
     const opts = {
       height: '390',
-      width: '640'
+      width: '640',
+      playerVars: {
+        autoplay: 1
+      }
     };
 
     return (
       <YouTube
         videoId={this.props.videoPlayer.videoId}
         opts={opts}
+        onReady={this.onReady}
       />
     );
   }
