06edfb6260d6420fcc521441ca5a5ab6ce1f1975
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 18:21:47 +0300

    Add button to SearchResult which add current video to playlist state

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class SearchResult extends Component {
+import { addVideoToPlaylist } from '../actions';
+
+export class SearchResult extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { addVideoToPlaylist, searchResult } = this.props;
+    addVideoToPlaylist(searchResult);
+  }
+
   render() {
     const { searchResult } = this.props;
 
     return (
       <li>
         {searchResult.title}
+        <button onClick={this.handleClick}>+</button>
       </li>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({addVideoToPlaylist}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchResult);
