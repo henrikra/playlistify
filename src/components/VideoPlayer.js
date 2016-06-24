6011c586d50433371d6f4f7ba4084c020322fc7f
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 23:28:12 +0300

    Set player state to pause when video ends

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import YouTube from 'react-youtube';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { pausePlayer } from '../actions';
+
 
 export class VideoPlayer extends Component {
   constructor(props) {
     super(props);
 
     this.onReady = this.onReady.bind(this);
+    this.onEnd = this.onEnd.bind(this);
     this.state = {player: null};
   }
 
@@ -24,6 +29,10 @@ export class VideoPlayer extends Component {
     this.setState({player: e.target});
   }
 
+  onEnd() {
+    this.props.pausePlayer();
+  }
+
   render() {
     const opts = {
       height: '390',
@@ -38,6 +47,7 @@ export class VideoPlayer extends Component {
         videoId={this.props.videoPlayer.videoId}
         opts={opts}
         onReady={this.onReady}
+        onEnd={this.onEnd}
       />
     );
   }
@@ -47,4 +57,8 @@ function mapStateToProps({ videoPlayer }) {
   return {videoPlayer};
 }
 
-export default connect(mapStateToProps)(VideoPlayer);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({pausePlayer}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
