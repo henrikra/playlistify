3db681632749a84ac8b9b11e4089468760bbfab6
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 23:14:13 +0300

    Add tests to test that button inside PlaylistItem has right text according to state

--- test/PlaylistItem-test.js
+++ test/PlaylistItem-test.js
@@ -7,6 +7,7 @@ import { PlaylistItem } from '../src/components/PlaylistItem';
 describe('PlaylistItem', () => {
   const minimumProps = {
     playlistItem: {
+      id: {videoId: 'firstVideoId'},
       snippet: {title: 'Video title 1'}
     },
     videoPlayer: {}
@@ -15,5 +16,23 @@ describe('PlaylistItem', () => {
   it('contains video title', () => {
     const wrapper = shallow(<PlaylistItem {...minimumProps} />);
     expect(wrapper).to.contain.text(minimumProps.playlistItem.snippet.title);
+  });
+
+  it('shows play button when PlaylistItem is not currently playing', () => {
+    const props = Object.assign({}, minimumProps, {
+      videoPlayer: {isPlaying: false, videoId: 'myId'}
+    });
+    const wrapper = shallow(<PlaylistItem {...props} />);
+
+    expect(wrapper).to.contain.text('Play');
+  });
+
+  it('shows pause button when PlaylistItem is currently playing', () => {
+    const props = Object.assign({}, minimumProps, {
+      videoPlayer: {isPlaying: true, videoId: 'firstVideoId'}
+    });
+    const wrapper = shallow(<PlaylistItem {...props} />);
+
+    expect(wrapper).to.contain.text('Pause');
   });
 });
