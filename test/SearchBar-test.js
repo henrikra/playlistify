d9acd267849df13721288c412de90f708ecba223
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 15:24:50 +0300

    Remove minimum props because redux-form is not used anymore

--- test/SearchBar-test.js
+++ test/SearchBar-test.js
@@ -5,8 +5,7 @@ import { shallow } from 'enzyme';
 import { SearchBar } from '../src/components/SearchBar';
 
 describe('SearchBar', () => {
-  const minimumProps = {fields: {}};
-  const wrapper = shallow(<SearchBar {...minimumProps} />);
+  const wrapper = shallow(<SearchBar />);
 
   it('contains submit button', () => {
     expect(wrapper.find('input[type="submit"]')).to.exist;
