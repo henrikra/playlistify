dbfbbcdec37ea52eb9f53ed5ea1cceb2dd1c853a
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 19:46:14 +0300

    Handle REMOVE_FROM_PLAYLIST action type in reducer Added also test for it

--- src/reducers/playlist.js
+++ src/reducers/playlist.js
@@ -5,6 +5,9 @@ export default function playlist(state = [], action) {
     case types.ADD_TO_PLAYLIST:
       return [...state, action.video];
       break;
+    case types.REMOVE_FROM_PLAYLIST:
+      return state.filter(video => video.id.videoId !== action.videoId);
+      break;
     default:
       return state;
   }
--- test/reducerPlaylist-test.js
+++ test/reducerPlaylist-test.js
@@ -4,24 +4,48 @@ import reducer from '../src/reducers/playlist';
 import types from '../src/constants/ActionTypes';
 
 describe('Playlist reducer', () => {
-  const action = {
-    type: types.ADD_TO_PLAYLIST,
-    video: {title: 'New video'}
-  };
 
-  it('returns the initial state', () => {
-    expect(reducer(undefined, {})).to.eql([]);
+  describe('ADD_TO_PLAYLIST', () => {
+    const action = {
+      type: types.ADD_TO_PLAYLIST,
+      video: {title: 'New video'}
+    };
+
+    it('returns the initial state', () => {
+      expect(reducer(undefined, {})).to.eql([]);
+    });
+
+    it('adds video to empty playlist', () => {
+      expect(reducer(undefined, action)).to.eql([{title: 'New video'}]);
+    });
+
+    it('adds video to already initialized playlist', () => {
+      const initialState = [{title: 'This video is already in playlist'}];
+      expect(reducer(initialState, action)).to.eql([
+        {title: 'This video is already in playlist'},
+        {title: 'New video'}
+      ]);
+    });
   });
 
-  it('handles ADD_TO_PLAYLIST', () => {
-    expect(reducer(undefined, action)).to.eql([{title: 'New video'}]);
-  });
+  describe('REMOVE_FROM_PLAYLIST', () => {
+    const action = {
+      type: types.REMOVE_FROM_PLAYLIST,
+      videoId: 'superId2'
+    };
 
-  it('handles ADD_TO_PLAYLIST with already initialized state', () => {
-    const initialState = [{title: 'This video is already in playlist'}];
-    expect(reducer(initialState, action)).to.eql([
-      {title: 'This video is already in playlist'},
-      {title: 'New video'}
-    ]);
+    it('removes video from playlist', () => {
+      const initialState = [
+        {id: {videoId: 'superId1'}},
+        {id: {videoId: 'superId2'}},
+        {id: {videoId: 'superId3'}}
+      ];
+      const expectedState = [
+        {id: {videoId: 'superId1'}},
+        {id: {videoId: 'superId3'}}
+      ];
+
+      expect(reducer(initialState, action)).to.eql(expectedState);
+    });
   });
 });
