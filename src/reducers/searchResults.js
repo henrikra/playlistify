73d4932231d67082de2909c4159882eab04249e0
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-24 15:00:25 +0300

    Fix reducer to return right new state

--- src/reducers/searchResults.js
+++ src/reducers/searchResults.js
@@ -3,7 +3,7 @@ import types from '../constants/ActionTypes';
 export default function searchResults(state = [], action) {
   switch (action.type) {
     case types.SEARCH_VIDEOS:
-      return [action.payload.data.items, ...state];
+      return action.payload.data.items;
       break;
     default:
       return state;
