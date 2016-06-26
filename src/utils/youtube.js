b7cc0e354bf379a4336bea93c8614d90aa019542
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-26 21:54:30 +0300

    Remove callback because it is not used anymore

--- src/utils/youtube.js
+++ src/utils/youtube.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const YOUTUBE_DATA_API_URL = 'https://www.googleapis.com/youtube/v3/search';
 const API_KEY = 'AIzaSyCPLAZUcdIklVIGjfU5_WemijkkkxVsErU';
 
-export function searchYouTube(searchTerm, callback) {
+export function searchYouTube(searchTerm) {
   const params = {
     type: 'video',
     part: 'snippet',
